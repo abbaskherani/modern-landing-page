b83e56289b32fde7fea8f3fabc0369201e8260d1
abbaskherani <abbaskherani1998@gmail.com>
2024-10-03 19:55:01 +0530

    email to

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -29,7 +29,7 @@ const PricingList = () => {
 
           <Button
             className="w-full mb-6"
-            href={item.price ? "/pricing" : "mailto:aounabbaskherani@gmail.com"}
+            href={item.price ? "/pricing" : "mailto:abbaskherani1998@gmail.com"}
             white={!!item.price}
           >
             {item.price ? "Get started" : "Contact us"}
